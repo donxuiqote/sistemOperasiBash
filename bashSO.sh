2b1ced2b3bc095b1307e776db4df98c3f05bbc00
Don Quixote <113412835+donxuiqote@users.noreply.github.com>
2022-12-20 21:05:23 +0700

    theNewFolder
    some feature changes

--- bashSO.sh
+++ bashSO.sh
@@ -4,24 +4,32 @@ theNewFolder(){
         read name
         mkdir $name
         echo "Your $name Folder has been made" 
-        echo "Choose: "
+        echo "Choose    : "
         echo "1. Make A New Folder Again"
         echo "2. Go To The Menu"
-        echo "3. Exit"
+        echo "3. See lists"
+        echo "4. Exit"
         echo "\nInput Your Choice:"
         read pilihan
         case $pilihan in
-        1) clear
-                theNewFolder;;
-        2) clear
-                theMenu;;
-        3) clear
-                exit;;
-        4) clear
+        1) theNewFolder;;
+        2) theMenu;;
+        3) echo -e "\n Your files and folders are shown below : \n"
+            ls
+            echo -e "\n\n Choose    :"
+            echo "1. Make A New Folder Again"
+            echo "2. Go To The Menu"
+            echo "3. See lists"
+            echo "4. Exit"
+            echo "\nInput Your Choice   :";;
+        4) exit;;
+        *) clear
                 echo "======================!!!======================"
                 echo "Option not available, please re-enter your choice !"
                 read dummyInput;
-                theNewFolder;;
+                theMenu;;      
+        esac
+}
                 
 }
 #theFilePermission(){}
